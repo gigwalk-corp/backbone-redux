9eb60d02a8c0dcebf7a2a4ce862ba8cef55432bf
elimak <info@elimak.com>
2016-09-08 17:15:45 -0700

    added syncModels to the default exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-export {syncCollection, syncCollections, buildReducers, buildEars} from './collection-tools';
+export {syncModels, syncCollection, syncCollections, buildReducers, buildEars} from './collection-tools';
 export {default as actionFabric} from './action-fabric';
 export {default as reducerFabric} from './reducer-fabric';
