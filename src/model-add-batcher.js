9a9a267cbc9d816c0d9fbfa061ee5c043c673d4e
Jorge Morante <jorge@morante.eu>
2016-09-12 13:09:24 +0200

    WIP

---
+++ src/model-add-batcher.js
@@ -0,0 +1,31 @@
+import defer from 'lodash.defer';
+
+/**
+ * This class is responsible of batching adds, so when a fetch happens, a
+ * single ADD action is triggered, instead of once per model.
+ */
+export default class ModelAddBatcher {
+  constructor({ handle }) {
+    this.models = [];
+    this.handle = handle;
+  }
+
+  add(model) {
+    this.flushAfter();
+    this.models.push(model);
+  }
+
+  flushAfter() {
+    if (this.models.length > 0) {
+      return;
+    }
+
+    defer(() => this.flush());
+  }
+
+  flush() {
+    console.log('FLUSH');
+    this.handle(this.models);
+    this.models = [];
+  }
+}
--- test/collection-tools-test.js
+++ test/collection-tools-test.js
@@ -1,9 +1,10 @@
 import test from 'tape';
 import Backbone from 'backbone';
+import defer from 'lodash.defer';
 import {createStore} from 'redux';
 import {syncCollections} from '../src/collection-tools';
 
-test('Syncing collection', t => {
+test.only('Syncing collection WTf', t => {
   t.test('default values', t => {
     const collection = new Backbone.Collection();
     const store = createStore(() => {});
@@ -16,38 +17,36 @@ test('Syncing collection', t => {
     const jane = new Backbone.Model({id: 1, name: 'Jane'});
     collection.add(jane);
 
-    t.deepEqual(
-      store.getState().people,
-      {
-        entities: [
-          {id: 1, name: 'Jane', __optimistic_id: jane.cid},
-        ],
-        by_id: {
-          1: {id: 1, name: 'Jane', __optimistic_id: jane.cid},
-        },
-      }
-    );
-
     // adding 2 models
     const mark = new Backbone.Model({id: 2, name: 'Mark'});
     const sophy = new Backbone.Model({id: 3, name: 'Sophy'});
     collection.add([mark, sophy]);
 
-    t.deepEqual(
-      store.getState().people,
-      {
-        entities: [
-          {id: 1, name: 'Jane', __optimistic_id: jane.cid},
-          {id: 2, name: 'Mark', __optimistic_id: mark.cid},
-          {id: 3, name: 'Sophy', __optimistic_id: sophy.cid},
-        ],
-        by_id: {
-          1: {id: 1, name: 'Jane', __optimistic_id: jane.cid},
-          2: {id: 2, name: 'Mark', __optimistic_id: mark.cid},
-          3: {id: 3, name: 'Sophy', __optimistic_id: sophy.cid},
-        },
-      }
-    );
+    // Batches add and handles them when the stack is cleared
+    defer(()=> {
+      t.deepEqual(
+        store.getState().people,
+        {
+          entities: [
+            {id: 1, name: 'Jane', __optimistic_id: jane.cid},
+            {id: 2, name: 'Mark', __optimistic_id: mark.cid},
+            {id: 3, name: 'Sophy', __optimistic_id: sophy.cid},
+          ],
+          by_id: {
+            1: {id: 1, name: 'Jane', __optimistic_id: jane.cid},
+            2: {id: 2, name: 'Mark', __optimistic_id: mark.cid},
+            3: {id: 3, name: 'Sophy', __optimistic_id: sophy.cid},
+          },
+        }
+      );
+    });
+
+    setTimeout(() => {
+      console.log('collection.toJSON()', collection.toJSON());
+      t.end();
+    }, 2000);
+    return;
+
 
     // changing models
     jane.set('name', 'Jennifer');
@@ -97,8 +96,12 @@ test('Syncing collection', t => {
       }
     );
 
-    t.end();
+    setTimeout(() => {
+      t.end();
+    }, 5000)
   });
+
+  return;
 
   t.test('initial sync', t => {
     const jane = new Backbone.Model({id: 1, name: 'Jane'});
